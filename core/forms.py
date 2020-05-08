c384dd7181074488f3614f594849abc9d7d64fd6
phuc <vanphucpro123@gmail.com>
2020-05-08 14:58:44 +0700

    first commit

---
+++ core/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+from django_countries.fields import CountryField
+from django_countries.widgets import CountrySelectWidget
+
+PAYMENT_CHOICES = (
+    ('S', 'Stripe'),
+    ('P', 'PayPal')
+)
+
+
+class CheckoutForm(forms.Form):
+    street_address = forms.CharField(widget=forms.TextInput(attrs={
+
+        'class': 'form-control'
+    }))
+    street_address = forms.CharField(widget=forms.TextInput(attrs={
+
+        'class': 'form-control'
+    }))
+
+    country = CountryField(blank_label='------------------').formfield(widget=CountrySelectWidget(attrs={
+        'class': 'custom-select d-block w-100'
+    }))
+
+    zip = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control'
+    }))
+    same_shipping_address = forms.BooleanField(required=False)
+    save_info = forms.BooleanField(required=False)
+    payment_option = forms.ChoiceField(
+        widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
+
+
+class CouponForm(forms.Form):
+    code = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+       'placeholder': 'Mã giảm giá'
+    }))
+
+class RefundForm(forms.Form):
+    ref_code = forms.CharField()
+    message = forms.CharField(widget=forms.Textarea(attrs={
+        'rows': 4
+    }))
+    email = forms.EmailField()
